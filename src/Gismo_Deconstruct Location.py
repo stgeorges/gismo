9c9e03c30c04508018e619b117b021e4a6b08b1f
Djordje Spasic <issworld2000@yahoo.com>
2017-03-02 00:23:39 +0100

    gismo 0.0.2 release update

--- src/Gismo_Deconstruct Location.py
+++ src/Gismo_Deconstruct Location.py
@@ -5,7 +5,6 @@
 # This file is part of Gismo.
 #
 # Copyright (c) 2017, Djordje Spasic <djordjedspasic@gmail.com>
-#
 # Gismo is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 #
 # Gismo is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -17,10 +16,10 @@
 """
 Use this component to exlode a location to its parts: locationName, latitude, longitude, timeZone, elevation.
 -
-Provided by Gismo 0.0.1
+Provided by Gismo 0.0.2
 
     input:
-        _location: And .epw file location: joined string containing information about location's components: locationName, latitude, longitude, timeZone and elevation.
+        _location: An .epw file location: joined string containing information about location's components: locationName, latitude, longitude, timeZone and elevation.
     
     output:
         readMe!: ...
@@ -31,15 +30,19 @@ Provided by Gismo 0.0.1
                    It ranges from -90 for locations south of equator, to 90 for locations above the equator.
         longitude: Location's longitude coordinate.
                     It ranges from -180 for locations west of Prime Meridian, to 180 for locations east of Prime Meridian.
+        timeZone: The time zone of the location.
+        elevation: Elevation (Altitude) height of the .epw location.
+                   -
+                   In meters.
 """
 
 ghenv.Component.Name = "Gismo_Deconstruct Location"
 ghenv.Component.NickName = "DeconstructLocation"
-ghenv.Component.Message = "VER 0.0.1\nFEB_24_2017"
+ghenv.Component.Message = "VER 0.0.2\nMAR_01_2017"
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Gismo"
 ghenv.Component.SubCategory = "0 | Gismo"
-#compatibleGismoVersion = VER 0.0.1\nJAN_29_2017
+#compatibleGismoVersion = VER 0.0.2\nMAR_01_2017
 try: ghenv.Component.AdditionalHelpFromDocStrings = "2"
 except: pass
 
@@ -79,3 +82,4 @@ if sc.sticky.has_key("gismoGismo_released"):
 else:
     printMsg = "First please run the Gismo Gismo component."
     print printMsg
+    ghenv.Component.AddRuntimeMessage(level, printMsg)
