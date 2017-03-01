da1081969f86878f8c8dd2a8a835ffd178d1950b
Djordje Spasic <issworld2000@yahoo.com>
2017-03-02 00:30:36 +0100

    gismo 0.0.2 release update

--- src/Gismo_Terrain Analysis.py
+++ src/Gismo_Terrain Analysis.py
@@ -43,7 +43,7 @@ http://gis.stackexchange.com/a/6059/65002
 http://gis4geomorphology.com/roughness-topographic-position
 https://github.com/wschwanghart/topotoolbox/blob/master/@GRIDobj/roughness.m
 -
-Provided by Gismo 0.0.1
+Provided by Gismo 0.0.2
     
     input:
         _analysisType: Choose one of the terrain analysis types:
@@ -118,11 +118,11 @@ Provided by Gismo 0.0.1
 
 ghenv.Component.Name = "Gismo_Terrain Analysis"
 ghenv.Component.NickName = "TerrainAnalysis"
-ghenv.Component.Message = "VER 0.0.1\nJAN_29_2017"
+ghenv.Component.Message = "VER 0.0.2\nMAR_01_2017"
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Gismo"
 ghenv.Component.SubCategory = "2 | Terrain"
-#compatibleGismoVersion = VER 0.0.1\nJAN_29_2017
+#compatibleGismoVersion = VER 0.0.2\nMAR_01_2017
 try: ghenv.Component.AdditionalHelpFromDocStrings = "2"
 except: pass
 
