80f35734d21692188754282d7151c49584c027cd
Djordje Spasic <issworld2000@yahoo.com>
2017-05-05 18:20:47 +0200

    Adding new OSMobject: "Steps"

--- src/Gismo_OSM Objects.py
+++ src/Gismo_OSM Objects.py
@@ -28,11 +28,11 @@ Provided by Gismo 0.0.2
 
 ghenv.Component.Name = "Gismo_OSM Objects"
 ghenv.Component.NickName = "OSMobjects"
-ghenv.Component.Message = "VER 0.0.2\nMAR_02_2017"
+ghenv.Component.Message = "VER 0.0.2\nMAY_05_2017"
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Gismo"
 ghenv.Component.SubCategory = "1 | OpenStreetMap"
-#compatibleGismoVersion = VER 0.0.2\nMAR_01_2017
+#compatibleGismoVersion = VER 0.0.2\nMAY_05_2017
 try: ghenv.Component.AdditionalHelpFromDocStrings = "2"
 except: pass
 
@@ -78,6 +78,7 @@ Casino                = "Casino"
 Road                  = "Road"
 Railway               = "Railway"
 Footway               = "Footway"
+Steps                 = "Steps"
 Pedestrian zone       = "Pedestrian zone"
 Aeroway               = "Aeroway"
 Bicycle parking       = "Bicycle parking"
