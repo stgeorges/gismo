5e07290715c925edcc4ef7ec0ced9361217b58ff
Djordje Spasic <issworld2000@yahoo.com>
2019-01-29 00:14:38 +0100

    Update code to Gismo 0.0.3 version

--- src/Gismo_Legend Bake Parameters.py
+++ src/Gismo_Legend Bake Parameters.py
@@ -4,7 +4,7 @@
 # 
 # This file is part of Gismo.
 # 
-# Copyright (c) 2017, Djordje Spasic <djordjedspasic@gmail.com>
+# Copyright (c) 2019, Djordje Spasic <djordjedspasic@gmail.com>
 # Gismo is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 #
 # Gismo is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -17,7 +17,7 @@
 Use this component to define the parameters important for legend and baking.
 Any component which has a "legendBakePar_" input can be used to define these.
 -
-Provided by Gismo 0.0.2
+Provided by Gismo 0.0.3
     input:
         legendStyle_: The following legend cell styles are supported:
                       -
@@ -80,11 +80,11 @@ Provided by Gismo 0.0.2
 
 ghenv.Component.Name = "Gismo_Legend Bake Parameters"
 ghenv.Component.NickName = "LegendBakeParameters"
-ghenv.Component.Message = "VER 0.0.2\nMAY_05_2017"
+ghenv.Component.Message = "VER 0.0.3\nJAN_29_2019"
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Gismo"
 ghenv.Component.SubCategory = "3 | More"
-#compatibleGismoVersion = VER 0.0.2\nMAy_05_2017
+#compatibleGismoVersion = VER 0.0.3\nJAN_29_2019
 try: ghenv.Component.AdditionalHelpFromDocStrings = "2"
 except: pass
 
