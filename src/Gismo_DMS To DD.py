8c2ba7880d4b72202055dddef2f5dc4968a72d83
Djordje Spasic <issworld2000@yahoo.com>
2019-01-29 00:11:49 +0100

    Update code to Gismo 0.0.3 version

--- src/Gismo_DMS To DD.py
+++ src/Gismo_DMS To DD.py
@@ -6,7 +6,7 @@
 #
 # This file is part of Gismo.
 #
-# Copyright (c) 2017, Djordje Spasic <djordjedspasic@gmail.com>
+# Copyright (c) 2019, Djordje Spasic <djordjedspasic@gmail.com>
 # Gismo is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 #
 # Gismo is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -19,7 +19,7 @@
 Use this component to convert a coordinate in degrees, minutes and seconds (DMS) format to decimal degrees (DD) format.
 This can be useful for Gismo component's "Create Location" inputs in case you have latitude/longitude coordinates in DMS instead of DD format.
 -
-Provided by Gismo 0.0.2
+Provided by Gismo 0.0.3
 
     input:
         _dms: A string representing degrees minutes and seconds (DMS) coordinate.
@@ -34,11 +34,11 @@ Provided by Gismo 0.0.2
 
 ghenv.Component.Name = "Gismo_DMS To DD"
 ghenv.Component.NickName = "DMSToDD"
-ghenv.Component.Message = "VER 0.0.2\nMAY_01_2017"
+ghenv.Component.Message = "VER 0.0.3\nJAN_29_2019"
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Gismo"
 ghenv.Component.SubCategory = "1 | Gismo"
-#compatibleGismoVersion = VER 0.0.2\nMAR_01_2017
+#compatibleGismoVersion = VER 0.0.3\nJAN_29_2019
 try: ghenv.Component.AdditionalHelpFromDocStrings = "2"
 except: pass
 
